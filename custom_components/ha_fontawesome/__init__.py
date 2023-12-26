cbde6c96f0577d17db6285d6b8cda82be7d2b5c8
WebDevNerdStuff <webdevnerdstuff@gmail.com>
2023-12-25 17:05:16 -0800

    setup

--- custom_components/ha_fontawesome/__init__.py
+++ custom_components/ha_fontawesome/__init__.py
@@ -5,14 +5,11 @@ import asyncio
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.typing import ConfigType
 
-
-
 DOMAIN = "ha_fontawesome"
 
 
 @asyncio.coroutine
-async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     hass.states.async_set('ha_fontawesome.Hello_World', 'Works!')
 
-    # Return boolean to indicate that initialization was successfully.
     return True
